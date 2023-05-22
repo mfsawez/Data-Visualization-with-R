a0479eb2faa211e87ec45e8ba82934a39fb8c2fc
Meena Sawez <113933575+mfsawez@users.noreply.github.com>
2023-05-22 16:38:10 -0700

    Create server.R

---
+++ server.R
@@ -0,0 +1,66 @@
+# Load libraries
+library(shiny)
+library(tidyverse)
+library(rio)
+library(ggthemes)
+library(rsconnect)
+
+# Read in data
+adult <- read_csv("adult.csv")
+# Convert column names to lowercase for convenience 
+names(adult) <- tolower(names(adult))
+
+# Define server logic
+server=shinyServer(function(input, output) {
+  adult=import("adult.csv")               # Read in data
+  names(adult)=tolower(names(adult))      # Convert column names to lowercase for convenience 
+  
+  df_country <- reactive({
+    adult %>% filter(native_country == input$country)
+  })
+  
+  # TASK 5: Create logic to plot histogram or boxplot
+  output$p1 <- renderPlot({
+    if (input$graph_type == "histogram") {
+      # Histogram
+      ggplot(df_country(), aes_string(x =input$continuous_variable)) +
+        geom_histogram(color="purple",fill="lavender") +  # histogram geom
+        labs(y="Number of People", title=paste("Trend of ",input$continuous_variable)) +  # labels
+        facet_wrap(~prediction)+ # facet by prediction
+        theme_light()
+    }
+    else {
+      # Boxplot
+      ggplot(df_country(), aes_string(y = input$continuous_variable)) +
+        geom_boxplot(color="black",fill="darkgreen") +  # boxplot geom
+        coord_flip() +  # flip coordinates
+        labs(x="Number of People", title=paste("Boxplot of",input$continuous_variable)) +  # labels
+        facet_wrap(~prediction)+    # facet by prediction
+        theme_light()
+    }
+    
+  })
+  
+  # TASK 6: Create logic to plot faceted bar chart or stacked bar chart
+  output$p2 <- renderPlot({
+    # Bar chart
+    p <- ggplot(df_country(), aes_string(x =input$categorical_variable)) +
+      labs(y="Number of People",title=(paste("Trend of",input$categorical_variable))) +  # labels
+      theme_light()+
+      theme(axis.text.x=element_text(angle=45),legend.position="bottom")    # modify theme to change text angle and legend position
+    
+    
+    if (input$is_stacked) {
+      p + geom_bar(aes(fill=prediction))  # add bar geom and use prediction as fill
+    }
+    else{
+      p + 
+        geom_bar(aes_string(fill=input$categorical_variable)) + # add bar geom and use input$categorical_variables as fill 
+        facet_wrap(~prediction)   # facet by prediction
+    }
+  })
+  
+})
+
+
+shinyApp(ui,server)
